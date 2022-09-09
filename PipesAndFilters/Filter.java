40f0f76830078b6237bb7b4c563d8a46a35491b6
teo <teodawn.td@gmail.com>
2022-09-09 16:44:35 -0500

    first version

--- PipesAndFilters/Filter.java
+++ PipesAndFilters/Filter.java
@@ -1,5 +1,7 @@
 package PipesAndFilters;
 
 public abstract class Filter {
+    private Pipe inPort;
+    private Pipe outPort;
 
 }
--- PipesAndFilters/Pipe.java
+++ PipesAndFilters/Pipe.java
@@ -1,5 +1,14 @@
 package PipesAndFilters;
 
 public class Pipe {
+    Object info;
+
+    public Object getInfo() {
+        return info;
+    }
+
+    public void setInfo(Object info) {
+        this.info = info;
+    }
 
 }
